82054291351ac6e0f4b91d83fd5a937fa6eee7c0
Aviral Kaushik <zaviralkaushik@gmail.com>
2023-06-30 16:16:57 +0530

    Fixed Some Bugs and added images

--- app/src/main/java/com/aviral/apwallet/AddMoneyFragment/CardDetailsFragment.java
+++ app/src/main/java/com/aviral/apwallet/AddMoneyFragment/CardDetailsFragment.java
@@ -1,17 +1,15 @@
 package com.aviral.apwallet.AddMoneyFragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.aviral.apwallet.Activities.AddMoneyActivity;
-import com.aviral.apwallet.R;
 import com.aviral.apwallet.databinding.FragmentCardDetailsBinding;
 
 public class CardDetailsFragment extends Fragment {
